612fff78080563b91bd73cae0ee7ece926472a4e
shafeeqonline <shafeeq.rahman01@gmail.com>
2017-09-08 11:36:20 -0400

    Adding initial POC code for child parent relations

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,7 +8,28 @@ angular.module('myApp.view1', ['ngRoute'])
     controller: 'View1Ctrl'
   });
 }])
-
 .controller('View1Ctrl', [function() {
-
-}]);
+  
+  }])
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.items = { list: [] };
+  
+  $http({
+    method: 'GET',
+    url: 'http://www.mocky.io/v2/59b2206a1200006a0189220f'
+  }).then(function(response) {
+    console.log(JSON.parse(response.data));
+    $scope.items.list = JSON.parse(response.data).items;
+  }, function() {
+    console.log('error');
+  });
+}])
+.component('childComp', {
+template: '<ul ng-repeat="item in items.list"><li>{{item.value}}</li></ul>',
+bindings: {
+  items: '<'
+},
+controller: ['$scope',function($scope) {
+  $scope.items = this.items;
+}]
+});
