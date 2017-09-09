66ad9c17d8fa264f64dfbb34151b7eedf4322c5f
Anup Mankar <amankar@sapient.com>
2017-09-08 22:10:48 -0400

    * Introduced a service that acts as a data store * Simplified code to prove concept * TODO: Try to add complexity to data/access

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,47 +5,52 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'MainCtrl'
   });
 }])
 .controller('View1Ctrl', [function() {
   
   }])
-.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-  $scope.items = { list: [] };
-  $scope.parentValue = "Parent Value";
-
-  this.updatevalue = function(newValues) {
-    console.log("Sfdsd", newValues)
-    for (var key in newValues) {
-      if (newValues.hasOwnProperty(key)) {
-        $scope[key] = newValues[key];
-      }
-    };
+.service('dataService', ['$http', function($http){
+  let svc = this;
+  this.items = {
+    list: []
   };
-  
-  $http({
-    method: 'GET',
-    url: 'http://www.mocky.io/v2/59b2206a1200006a0189220f'
-  }).then(function(response) {
-    $scope.items.list = JSON.parse(response.data).items;
-  }, function() {
-    console.log('error');
-  });
+
+  /** Fetch items from an external service and set the store */
+  this.getItems = function() {
+    $http({
+      method: 'GET',
+      url: 'http://www.mocky.io/v2/59b2206a1200006a0189220f'
+    }).then(function(response) {
+      console.log(response);
+      svc.updateItems(JSON.parse(response.data).items);
+    }, function() {
+      console.log('error');
+    });
+  };
+
+  /** Update the values in the global store */
+  this.updateItems = function(values) {
+    this.items.list = values;
+  };
+
+}])
+.controller('MainCtrl', ['$scope', 'dataService', function($scope, dataService) {
+  $scope.items = dataService.items;
+  this.update = function(values) {
+    dataService.updateItems(values);
+  };
+  dataService.getItems();
 }])
 .component('childComp', {
-template: '<ul ng-repeat="item in items.list"><input ng-model="item.value"></input></ul><input ng-blur="$ctrl.changeParent(parentvalue)" ng-model="parentvalue"></input>',
-bindings: {
-  items: '<',
-  onChange: '=',
-  parentvalue: '='
-},
-controller: ['$scope',function($scope) {
-  var ctrl = this;
-  $scope.items = this.items;
-  $scope.parentvalue = this.parentvalue;
-  ctrl.changeParent = function(value) {
-    ctrl.onChange({parentValue: value});
-  }
-}]
+  template: '<ul ng-repeat="item in items.list"><input ng-model="item.value"></input></ul>',
+  bindings: {
+    items: '<',
+    onChange: '&'
+  },
+  controller: ['$scope',function($scope) {
+    var ctrl = this;
+    $scope.items = this.items;
+  }]
 });
