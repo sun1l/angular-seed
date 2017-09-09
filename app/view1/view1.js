291736b436f502d1b3e0a88dbd56fa0151da12b1
shafeeqonline <shafeeq.rahman01@gmail.com>
2017-09-08 20:36:15 -0400

    Child updates parent via reference & methods

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,23 +13,39 @@ angular.module('myApp.view1', ['ngRoute'])
   }])
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.items = { list: [] };
+  $scope.parentValue = "Parent Value";
+
+  this.updatevalue = function(newValues) {
+    console.log("Sfdsd", newValues)
+    for (var key in newValues) {
+      if (newValues.hasOwnProperty(key)) {
+        $scope[key] = newValues[key];
+      }
+    };
+  };
   
   $http({
     method: 'GET',
     url: 'http://www.mocky.io/v2/59b2206a1200006a0189220f'
   }).then(function(response) {
-    console.log(JSON.parse(response.data));
     $scope.items.list = JSON.parse(response.data).items;
   }, function() {
     console.log('error');
   });
 }])
 .component('childComp', {
-template: '<ul ng-repeat="item in items.list"><li>{{item.value}}</li></ul>',
+template: '<ul ng-repeat="item in items.list"><input ng-model="item.value"></input></ul><input ng-blur="$ctrl.changeParent(parentvalue)" ng-model="parentvalue"></input>',
 bindings: {
-  items: '<'
+  items: '<',
+  onChange: '=',
+  parentvalue: '='
 },
 controller: ['$scope',function($scope) {
+  var ctrl = this;
   $scope.items = this.items;
+  $scope.parentvalue = this.parentvalue;
+  ctrl.changeParent = function(value) {
+    ctrl.onChange({parentValue: value});
+  }
 }]
 });
